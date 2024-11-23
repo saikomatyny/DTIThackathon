42f47ef23d98d56f47b449ff5acfb74b4b870880
zblednu <maksym.surovtsev@student.tuke.sk>
2024-11-23 17:17:20 +0100

    working frontend

--- front-end/script.js
+++ front-end/script.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 "use strict"
 const leftPane = document.querySelector(".left-pane");
 const rightPane = document.querySelector(".right-pane");
@@ -6,10 +5,6 @@ const payload = {
   userFile: "",
   templateFile: ""
 };
-=======
-const leftPane = document.querySelector(".left_pane");
-const rightPane = document.querySelector(".right_pane");
->>>>>>> 0c5bb942c4939570687d1b09bc6d3b768bfad1ed
 
 ['dragenter', 'dragover', 'dragleave', 'drop'].forEach(eventName => {
   leftPane.addEventListener(eventName, preventDefaults, true );
@@ -22,7 +17,6 @@ function preventDefaults(e) {
 }
 
 [leftPane, rightPane].forEach(pane => {
-<<<<<<< HEAD
   pane.addEventListener("drop", handleDrop, true);
 });
 
@@ -87,47 +81,3 @@ button.addEventListener("click", () => {
 });
 
 
-=======
-    pane.addEventListener("dragenter", handleDragEnter, false);
-    pane.addEventListener("drop", handleDrop, false);
-    pane.addEventListener("dragleave", handleDragLeave, false);
-});
-
-function handleDragEnter(event) {
-    const pane = event.currentTarget;
-
-    if (!pane.classList.contains("drag-active")) {
-        pane.style.backgroundColor = "rgba(142, 37, 100, 0.5)";
-    }
-}
-
-function handleDragLeave(event) {
-    const pane = event.currentTarget;
-
-    pane.style.backgroundColor = "rgba(0, 0, 0, 0.3)";
-}
-
-function handleDrop(event) {
-    const pane = event.currentTarget;
-    const files = event.dataTransfer.files;
-
-    if (files.length > 0) {
-        const file = files[0];
-        if (file.type === 'application/pdf') {
-            const fileURL = URL.createObjectURL(file);
-
-            const iframe = document.createElement("iframe");
-            iframe.src = fileURL;
-            iframe.style.width = "95%";
-            iframe.style.height = "95%";
-            iframe.style.zIndex = "-1";
-
-            pane.innerHTML = '';
-            pane.appendChild(iframe);
-        } else {
-            pane.innerHTML = '<p>Please drop a valid PDF file.</p>';
-        }
-    }
-    pane.style.backgroundColor = "transparent";
-}
->>>>>>> 0c5bb942c4939570687d1b09bc6d3b768bfad1ed
